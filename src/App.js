ae408fbf81ada5df59286f9a4c048f8ec6c8c427
Jeremy <jeremyVDLRC@gmail.com>
2020-09-27 23:14:23 +1000

    search functionality working

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './App.css';
 import Header from './components/Header';
 import SearchBar from './components/SearchBar';
@@ -7,14 +7,32 @@ import sampleEmployees from './sampleEmployees.json';
 
 
 function App(){
+ 
+ 
+  
+const [employeeResults] = React.useState(sampleEmployees);
+const [searchValue, setSearchValue] = React.useState('');
 
+const handleSearch = event => {
+  setSearchValue(event.target.value);
+}
+
+
+const searchResults = employeeResults.filter(result =>{
+  const searchfirstName = result.firstName.toLowerCase().includes(searchValue.toLowerCase());
+  const searchlastName = result.lastName.toLowerCase().includes(searchValue.toLowerCase());
+  const searchDepartment = result.department.toLowerCase().includes(searchValue.toLowerCase());
+  const searchEmail = result.email.toLowerCase().includes(searchValue.toLowerCase());
+  
+  return searchfirstName || searchlastName || searchDepartment || searchEmail;
+})
 
   return(
     <div>
 <Header/>
-<SearchBar/>
+<SearchBar onInput={handleSearch}  />
 
-<Table employees= {sampleEmployees}  />
+<Table employees= {searchResults}  />
 
 
 </div>
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,65 +1,28 @@
-import React, { Component } from "react";
+import React from "react";
 
-class SearchBar extends Component {
-  // Setting the component's initial state
-  state = {
-    firstName: "",
-    lastName: ""
-  };
+function SearchBar(props) {
+ 
 
-  handleInputChange = event => {
-    // Getting the value and name of the input which triggered the change
-    const { name, value } = event.target;
-
-    // Updating the input's state
-    this.setState({
-      [name]: value
-    });
-  };
-
-  handleFormSubmit = event => {
-    // Preventing the default behavior of the form submit (which is to refresh the page)
-    event.preventDefault();
-
-    // Alert the user their first and last name, clear `this.state.firstName` and `this.state.lastName`, clearing the inputs
-    alert(`Searching for ${this.state.firstName} ${this.state.lastName}`);
-    this.setState({
-      firstName: "",
-      lastName: ""
-    });
-  };
-
-  render() {
-    // Notice how each input has a `value`, `name`, and `onChange` prop
-    return (
-      <div>
-        <p>
-         
-        </p>
+      return (
+      
+    <div>
         <form className="form">
           <input
-            value={this.state.firstName}
-            name="firstName"
-            onChange={this.handleInputChange}
+            value={props.searchValue}
+            name="searchValue"
+            onChange={props.onInput}
             type="text"
-            placeholder="First Name"
+            placeholder="Search Employees"
           />
-          <input
-            value={this.state.lastName}
-            name="lastName"
-            onChange={this.handleInputChange}
-            type="text"
-            placeholder="Last Name"
-          />
-          <button onClick={this.handleFormSubmit}>Submit</button>
+         
         </form>
 
+        </div>
+        
 
-        You searched: {this.state.firstName} {this.state.lastName}
-
-      </div>
+     
     );
-  }
-}
+      }
+
 
 export default SearchBar;
